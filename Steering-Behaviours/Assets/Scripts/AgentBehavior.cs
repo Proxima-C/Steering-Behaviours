6106bc601f147d9edbde794560894c3010ae0ccd
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-12-27 20:51:51 +0200

    Update AgentBehavior script

--- Steering-Behaviours/Assets/Scripts/AgentBehavior.cs
+++ Steering-Behaviours/Assets/Scripts/AgentBehavior.cs
@@ -5,6 +5,27 @@ public class AgentBehavior : MonoBehaviour
     public GameObject target;
     protected Agent agent;
 
+    public float MapToRange(float rotation)
+    {
+        rotation %= 360.0f;
+        if (Mathf.Abs(rotation) > 180.0f)
+        {
+            if (rotation < 0.0f)
+                rotation += 360.0f;
+            else
+                rotation -= 360.0f;
+        }
+        return rotation;
+    }
+
+    public Vector3 OriToVec(float orientation)
+    {
+        Vector3 vector = Vector3.zero;
+        vector.x = Mathf.Sin(orientation * Mathf.Deg2Rad) * 1.0f;
+        vector.z = Mathf.Cos(orientation * Mathf.Deg2Rad) * 1.0f;
+        return vector.normalized;
+    }
+
     public virtual void Awake()
     {
         agent = gameObject.GetComponent<Agent>();
