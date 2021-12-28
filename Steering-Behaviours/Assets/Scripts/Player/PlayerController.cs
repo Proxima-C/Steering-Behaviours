d9bb5acea2cee81ef7754c3d4b3894fad38b246a
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-12-28 02:37:52 +0200

    Add shoot ability

--- Steering-Behaviours/Assets/Scripts/Player/PlayerController.cs
+++ Steering-Behaviours/Assets/Scripts/Player/PlayerController.cs
@@ -2,18 +2,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private float force = 5;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private float playerSpeed = 2.0f;
+    [SerializeField] private int bulletsCount = 10;
 
-    private Agent vehicle;
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private Transform shootOrigin;
+    [SerializeField] private float launchVelocity = 700f;
 
     private void Awake()
     {
-        vehicle = GetComponent<Agent>();
+        controller = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        vehicle.ApplyForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * force);
+        Move();
+        Shoot();
+    }
+
+    private void Move()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+        
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1") && bulletsCount > 0)
+        {
+            GameObject bullet = Instantiate(projectile, shootOrigin.position, shootOrigin.rotation);
+            bullet.GetComponentInChildren<Rigidbody>().AddForce(shootOrigin.forward * launchVelocity);
+            bulletsCount--;
+        }
     }
 }
