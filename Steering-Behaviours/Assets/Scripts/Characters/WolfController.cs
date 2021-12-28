6020e85cbcf863a688b4f6b2904b9b3ba6a775fe
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-12-28 04:28:38 +0200

    Update WolfController

--- Steering-Behaviours/Assets/Scripts/Characters/WolfController.cs
+++ Steering-Behaviours/Assets/Scripts/Characters/WolfController.cs
@@ -1,9 +1,15 @@
+using System.Collections;
 using System.Linq;
 using UnityEngine;
 
 public class WolfController : MonoBehaviour, IDamageable
 {
+    [SerializeField] float hungerMax = 100f;
+    [SerializeField] float hungerCurrent;
+    [SerializeField] private float hungerDecrease = 1f;
+
     [SerializeField] private float radius;
+    [SerializeField] private float attackRadius;
 
     private Agent agent;
     private Wander wander;
@@ -17,12 +23,25 @@ public class WolfController : MonoBehaviour, IDamageable
         wander = gameObject.GetComponent<Wander>();
         seek = gameObject.GetComponent<Seek>();
 
+        hungerCurrent = hungerMax;
+
         velocity = agent.VelocityLimit;
     }
 
     void Update()
     {
         HandleAnimalsChasing();
+        HandleHunger();
+    }
+
+    private void HandleHunger()
+    {
+        hungerCurrent = Mathf.Clamp(hungerCurrent - (hungerDecrease * Time.deltaTime), 0.0f, hungerMax);
+
+        if (hungerCurrent <= 0)
+        {
+            Damage();
+        }
     }
 
     private void HandleAnimalsChasing()
@@ -34,6 +53,12 @@ public class WolfController : MonoBehaviour, IDamageable
             wander.Weight = 0;
 
             agent.VelocityLimit = velocity * 1.5f;
+
+            if ((GetClosestAnimal().transform.position - transform.position).magnitude < attackRadius)
+            {
+                GetClosestAnimal().transform.root.GetComponent<IDamageable>()?.Damage();
+                hungerCurrent = hungerMax;
+            }
         }
         else
         {
@@ -71,6 +96,9 @@ public class WolfController : MonoBehaviour, IDamageable
     {
         Gizmos.color = Color.cyan;
         Gizmos.DrawWireSphere(transform.position, radius);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
     }
 
     public void Damage()
